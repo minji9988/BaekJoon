0d81f12b3455b1bdf7ecc653e5a3db489cabc2b1
Buang <104492622+minji9988@users.noreply.github.com>
2023-03-22 14:24:53 +0900

    Create README.md

---
+++ loop/git_three.java
@@ -0,0 +1,43 @@
+//
+//
+//        두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//
+//        첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+// 이상현상
+
+package loop;
+
+import java.util.Scanner;
+
+public
+class git_three {
+    public static
+    void main ( String[] args ) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int count = scan.nextInt(); // 몇 번 반복할 건지 입력
+        int [] result = new int[count];
+~
+
+        //입력된 숫자만큼 더해줌
+        for (int i=0; i<count; i++){
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+
+            result[i] = A + B;
+
+        }
+
+        // 입력된 숫자만큼 출력
+        for (int i=0; i<count; i++){
+            System.out.println(result[i]);
+
+        }
+
+
+
+    }
+
+}
